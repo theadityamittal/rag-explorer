b695a700678061d48e3ab4e9c1d03ebe0de8964f
Aditya Mittal <theadityamittal@gmail.com>
2025-09-10 12:34:18 -0400

    upgrade actions/upload-artifact from v3 to v4

--- src/support_deflect_bot/_version.py
+++ src/support_deflect_bot/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.0.post13'
-__version_tuple__ = version_tuple = (0, 1, 0, 'post13')
+__version__ = version = '0.1.0.post14'
+__version_tuple__ = version_tuple = (0, 1, 0, 'post14')
 
-__commit_id__ = commit_id = 'g56825c519'
+__commit_id__ = commit_id = 'gc8f15bb72'
